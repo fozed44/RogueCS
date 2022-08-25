b932674e878a92f5a53dd955c6f661297d3ab171
jerem <jerem@DESKTOP-V63VCH6>
2022-08-24 20:56:33 -0500

    Conversion of assets support.

---
+++ RogueCS/Core/Implementations/Assets.cs
@@ -0,0 +1,158 @@
+﻿using RogueCS.Core.Interfaces;
+using System.ComponentModel.DataAnnotations;
+using System.Reflection;
+
+namespace RogueCS.Core.Implementations {
+    public class Assets : IAssets {
+
+      #region constants
+
+        /// <summary>
+        /// Asset directory's, relative to the executable
+        /// directory.
+        const string ASSET_SUB_DIRECTORY = "assets";
+        
+        /// <summary>
+        /// json map data, relative to the asset directory.
+        /// </summary>
+        const string JSON_MAP_SUB_DIRECTORY   = "maps\\json";
+        
+        /// <summary>
+        /// fonts, relative to the asset directory.
+        /// </summary>
+        const string FONT_SUB_DIRECTORY = "fonts";
+        
+        /// <summary>
+        /// tilesheet json data, relative to the assets directory. (see TileSheet.h)
+        /// </summary>
+        const string TILE_SHEET_JSON_SUB_DIRECTORY = "tile-sheet\\json";
+        
+        /// <summary>
+        /// tilesheet BMP, relative to the assets directory. (see TileSheet.h)
+        /// </summary>
+        const string TILE_SHEET_BMP_SUB_DIRECTORY = "tile-sheet\\bmp";
+        
+        /// <summary>
+        /// Location of world data, relative to the assets directory.
+        /// </summary>
+        const string WORLDS_SUB_DIRECTORY = "worlds";
+        
+        /// <summary>
+        /// Location of compiled tile sheet json data, relative to the worlds directory.
+        /// {world-name} will be replaced with the name of the world.
+        /// </summary>
+        const string COMPILED_TILE_SHEET_JSON_SUB_DIRECTORY = "tile-sheet\\json";
+        
+        /// <summary>
+        /// Location of compiled tile sheet BMP data, relative to the worlds directory.
+        /// {world-name} will be replaced with the name of the world.
+        /// </summary>
+        const string COMPILED_TILE_SHEET_BMP_SUB_DIRECTORY = "tile-sheet\\png";
+
+      #endregion constants
+
+      #region ctor
+
+        public Assets() { }
+
+      #endregion ctor
+
+      #region IAssets
+
+        public IEnumerable<string> EnumerateTileSets() {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<string> EnumerateWorlds() {
+            return
+                Directory.EnumerateDirectories(
+                    GetAssetDirectory(BasicAsset.WORLD),
+                    "*",
+                    SearchOption.TopDirectoryOnly
+                );
+        }
+
+        public string GetAssetDirectory(BasicAsset assetType) {
+ 
+            switch (assetType) {
+                case BasicAsset.EXECUTABLE:
+                    return Path.GetDirectoryName(
+                        Assembly.GetEntryAssembly()!.Location
+                    )!;
+                case BasicAsset.ASSETS:
+                    return Path.Combine(
+                        GetAssetDirectory(BasicAsset.EXECUTABLE),
+                        ASSET_SUB_DIRECTORY
+                    );
+                case BasicAsset.JSON_MAP:
+                    return Path.Combine(
+                        GetAssetDirectory(BasicAsset.ASSETS),
+                        JSON_MAP_SUB_DIRECTORY
+                    );
+                case BasicAsset.FONT:
+                    return Path.Combine(
+                        GetAssetDirectory(BasicAsset.ASSETS),
+                        FONT_SUB_DIRECTORY
+                    );
+                case BasicAsset.WORLD:
+                    return Path.Combine(
+                        GetAssetDirectory(BasicAsset.ASSETS),
+                        WORLDS_SUB_DIRECTORY
+                    );
+                default:
+                    throw new ValidationException(
+                        $"invalid asset type {assetType}"
+                    );
+            }
+        }
+
+        public string GetAssetPathname(BasicAsset assetType, string assetName) {
+            return Path.Combine(
+                GetAssetDirectory(assetType),
+                assetName
+            );
+        }
+
+        public IEnumerable<string> GetAssetPaths(BasicAsset assetType) {
+            return Directory.EnumerateFiles(
+                GetAssetDirectory(assetType)
+            );
+        }
+
+        public string GetWorldAssetDirectory(string worldName, WorldAsset worldAsset) {
+            switch (worldAsset) {
+                case WorldAsset.COMPILED_TILE_SHEET_BMP:
+                    return Path.Combine(
+                        GetWorldDirectory(worldName),
+                        COMPILED_TILE_SHEET_JSON_SUB_DIRECTORY
+                    );
+
+                case WorldAsset.COMPILED_TILE_SHEET_JSON:
+                    return Path.Combine(
+                        GetWorldDirectory(worldName),
+                        COMPILED_TILE_SHEET_JSON_SUB_DIRECTORY
+                    );
+                default:
+                    throw new ValidationException(
+                        $"invalid WorldAsset type {worldAsset}"
+                    );
+            }
+        }
+
+        public string GetWorldAssetPathname(string worldName, WorldAsset worldAsset, string assetName) {
+            return Path.Combine(
+                GetWorldAssetDirectory(worldName, worldAsset),
+                assetName
+            );
+        }
+
+        public string GetWorldDirectory(string worldName) {
+            return Path.Combine(
+                GetAssetDirectory(BasicAsset.WORLD),
+                worldName
+            );
+        }
+
+        #endregion IAssets
+    }
+}
---
+++ RogueCS/Core/Interfaces/IAssets.cs
@@ -0,0 +1,34 @@
+﻿namespace RogueCS.Core.Interfaces {
+
+
+	/// <summary>
+	/// asset_type is used to identify various kinds of assets 
+	/// </summary>
+	public enum BasicAsset {
+		EXECUTABLE,      // Path to the executable
+		ASSETS,          // base directory for assets
+		JSON_MAP,        // directory containing json based maps
+		FONT,            // directory containing fonts
+		WORLD,           // directory containing saved world data
+		TILE_SET,
+	};
+
+	/// <summary>
+	/// Assets that are stored for a saved world.
+	/// </summary>
+	public enum WorldAsset {
+		COMPILED_TILE_SHEET_JSON,
+		COMPILED_TILE_SHEET_BMP
+	};
+
+    public interface IAssets {
+		string GetAssetDirectory(BasicAsset assetType);
+		IEnumerable<string> GetAssetPaths(BasicAsset assetType);
+		string GetAssetPathname(BasicAsset assetType, string assetName);
+		string GetWorldDirectory(string worldName);
+		string GetWorldAssetDirectory(string worldName, WorldAsset worldAsset);
+		string GetWorldAssetPathname(string worldName, WorldAsset worldAsset, string assetName);
+		IEnumerable<string> EnumerateWorlds();
+		IEnumerable<string> EnumerateTileSets();
+    }
+}
